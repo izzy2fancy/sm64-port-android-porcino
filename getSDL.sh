a010b93ccc7eec9b9914dda12317909139dc010d
izzy2fancy <121840901+izzy2fancy@users.noreply.github.com>
2024-01-23 17:14:00 -0500

    Update SDL version

--- getSDL.sh
+++ getSDL.sh
@@ -2,11 +2,11 @@
 
 pushd SDL
 
-wget https://www.libsdl.org/release/SDL2-2.0.12.zip
-unzip -q SDL2-2.0.12.zip
-mv SDL2-2.0.12/include include
+wget https://github.com/libsdl-org/SDL/releases/download/release-2.26.5/SDL2-2.26.5.zip
+unzip -q SDL2-2.26.5.zip
+mv SDL2-2.26.5/include include
 ln -s . include/SDL2
-rm SDL2-2.0.12.zip
-rm -r SDL2-2.0.12
+rm SDL2-2.26.5.zip
+rm -r SDL2-2.26.5
 
 popd
